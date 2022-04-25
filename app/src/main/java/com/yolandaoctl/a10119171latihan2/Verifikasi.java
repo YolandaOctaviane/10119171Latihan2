a8fb519a98475b6badcf19b0c370d0a42fa1e106
yolanda octaviane <73147428+YolandaOctaviane@users.noreply.github.com>
2022-04-25 14:53:43 +0700

    verif & verif  code page

--- app/src/main/java/com/yolandaoctl/a10119171latihan2/Verifikasi.java
+++ app/src/main/java/com/yolandaoctl/a10119171latihan2/Verifikasi.java
@@ -1,6 +1,22 @@
 package com.yolandaoctl.a10119171latihan2;
 
 
-//23 April 2022 10119171 Yolanda Octaviane IF5
-public class Verifikasi {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//25 April 2022 10119171 Yolanda Octaviane IF5
+public class Verifikasi extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.verifikasi);
+    }
+
+    public void verify (View view) {
+        Intent intent = new Intent(this, Verifikasi_code class);
+        startActivity(intent);
+    }
 }
