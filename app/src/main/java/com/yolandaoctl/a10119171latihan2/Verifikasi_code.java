5a3756adc31ef7fb885fec95f847541b38e28831
yolanda octaviane <73147428+YolandaOctaviane@users.noreply.github.com>
2022-04-26 13:23:53 +0700

    rev

--- app/src/main/java/com/yolandaoctl/a10119171latihan2/Verifikasi_code.java
+++ app/src/main/java/com/yolandaoctl/a10119171latihan2/Verifikasi_code.java
@@ -1,6 +1,21 @@
 package com.yolandaoctl.a10119171latihan2;
 
-//23 April 2022 10119171 Yolanda Octaviane IF5
-public class Verifikasi_code {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+    //25 April 2022 10119171 Yolanda Octaviane IF5
+public class Verifikasi_code extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.verifikasi_code);
+    }
+
+    public void send (View view) {
+        Intent intent = new Intent(this, user.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/yolandaoctl/a10119171latihan2/user.java
+++ app/src/main/java/com/yolandaoctl/a10119171latihan2/user.java
@@ -1,4 +1,13 @@
 package com.yolandaoctl.a10119171latihan2;
 
-public class user {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class user extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+    }
 }
